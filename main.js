77be9b6c0255f333e0ef00d941918bb94f8e1b6b
BRADEN <banana.spice@yahoo.com>
2017-12-04 08:50:36 -0600

    Commit Message

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 const Menu = electron.Menu;
 const ipc = electron.ipcMain
+
+app.on("ready", _=>{
+    console.log("ret-2-go!");
+    mainWindow = new BrowserWindow({ width:300, height:500});
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+    const menu = Menu.buildFromTemplate(myTemplate);
+    Menu.setApplicationMenu(menu);
+
+    mainWindow.on("closed", _=>{
+        mainWindow = null;
+    });
+});
+
+const myTemplate = [{
+    label: "File",
+    click: _=>{
+        },
+    }
+]
